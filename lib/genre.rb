06930af9c3d483b4367ef07c188f78292160391c
Gigi M <mmgamao1@gmail.com>
2019-03-31 18:23:38 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -30,5 +30,30 @@ def artists
   genre.all.collect {|song| song.genre
 
 
+class Genre
+  attr_accessor :name
 
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def songs
+    Song.all { |song| song.genre == self }
+  end
+
+  def artists
+    songs.map(&:artist)
+  end
+
+  def add_song(song)
+    songs << song
+  end
+end
 
