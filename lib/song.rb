7bf64ea5b4b23af59d99eb88564c9d5e2a64fb53
Gigi M <mmgamao1@gmail.com>
2019-03-31 18:09:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,7 @@
 //The Song model:
 // 1)The Song class needs a class variable @@all that begins as an empty array.
-// 2) A song should be initialized with a name, an artist, and a genre, and be saved in the @@all array.
+// 2) A song should be initialized with a name, an artist, and a genre, and be saved
+// in the @@all array.
 
 class Song
   attr_accessor :title, :artist
@@ -10,7 +11,8 @@ class Song
     @@all = []
   end  
 
-// 3)The Song class needs a class method .all that lists each song in the class variable.
+// 3)The Song class needs a class method .all that lists each song in the class 
+// variable.
 
 def all(song)
   @@all << self  
