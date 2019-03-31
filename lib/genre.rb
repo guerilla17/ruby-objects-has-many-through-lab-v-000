0e5f11eaddaddfba780c8c101be0ef325c048ccc
Gigi M <mmgamao1@gmail.com>
2019-03-31 18:36:02 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,26 +1,26 @@
-//The Genre model:
+# //The Genre model:
 
-//The Genre class needs a class variable @@all that begins as an empty array.
+# //The Genre class needs a class variable @@all that begins as an empty array.
 
 class Genre
   attr_accessor :name
 
   @@all = []
   
-//A genre should be initialized with a name and be saved in the @@all array.
+# //A genre should be initialized with a name and be saved in the @@all array.
 
   def initialize(name)
     @name = name
     @@all << self
   end
   
-//The Genre class needs a class method .all that lists each genre in the class variable.
+# //The Genre class needs a class method .all that lists each genre in the class variable.
 
 def self.all
   @@all
 end
 
-//The Genre class needs an instance method, #songs, that iterates through all songs and finds the songs that belong to that genre.
+# //The Genre class needs an instance method, #songs, that iterates through all songs and finds the songs that belong to that genre.
 
 def songs
   Song.all {|song| song.genre == self}
