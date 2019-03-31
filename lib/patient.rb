4bf238be527fb2966d18e7b92f87baa5e5082f72
Gigi M <mmgamao1@gmail.com>
2019-03-31 19:29:35 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -23,8 +23,13 @@ end
 # The Patient class needs an instance method, #new_appointment, that takes in an argument of a doctor and a date and creates a new Appointment. The Appointment should know that it belongs to the patient.
 
 def new_appointment(doctor, date)
-  Appointment.new()
+  Appointment.new(doctor, date, patient)
+end
+
 # The Patient class needs an instance method, #appointments, that iterates through the Appointments array and returns Appointments that belong to the patient.
+
+def appointments
+  
 
 # # The Patient class needs an instance method, #doctors, that iterates over that patient's Appointments and collects the doctor that belongs to each Appointment.
 
