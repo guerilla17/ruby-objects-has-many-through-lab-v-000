e112f61d03723a1f90c708049c9c4c962a13b0fe
Gigi M <mmgamao1@gmail.com>
2019-03-31 18:45:15 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,6 +1,6 @@
-The Appointment model:
+# The Appointment model:
 
-The Appointment class needs a class variable @@all that begins as an empty array.
+# The Appointment class needs a class variable @@all that begins as an empty array.
 
 
 class Appointment
@@ -8,18 +8,28 @@ class Appointment
 
   @@all = []
   
-The Appointment class needs a class method .all that lists each Appointment in the class variable.
-An Appointment should be initialized with a date (as a string, like "Monday, August 1st"), a patient, and a doctor. The Appointment should be saved in the @@all array.
-
-_____________
-
-
-  def initialize(patient, doctor, date)
+  # An Appointment should be initialized with a date (as a string, like "Monday, August 1st"), a patient, and a doctor. The Appointment should be saved in the @@all array.
+  
+    def initialize(patient, doctor, date)
     @date = date
     @doctor = doctor
     @patient = patient
     @@all << self
   end
+  
+# The Appointment class needs a class method .all that lists each Appointment in the class variable.
+
+def self.all
+  @@all 
+end
+end 
+
+
+
+_____________
+
+
+
 
   def self.all
     @@all
