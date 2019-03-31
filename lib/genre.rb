9a1702affa69b51e032cbdee2bad0882429554e9
Gigi M <mmgamao1@gmail.com>
2019-03-31 18:36:14 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -26,7 +26,7 @@ def songs
   Song.all {|song| song.genre == self}
 end
 
-//The Genre class needs an instance method, #artists, that iterates over the //genre's collection of songs and collects the artist that owns each song.
+#//The Genre class needs an instance method, #artists, that iterates over the //genre's collection of songs and collects the artist that owns each song.
 
 def artists
   songs.map(&:artist)
